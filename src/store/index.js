96efa13fcaabf14a15941a3dab4cc7bf1208d3ad
Lovish Tater <lovishtater@gmail.com>
2022-04-23 21:35:18 +0530

    removed old Architecture

--- src/store/index.js
+++
@@ -1,8 +0,0 @@
-import { createStore } from 'redux';
-import reducer from './reducer';
-
-//-----------------------|| REDUX - MAIN STORE ||-----------------------//
-
-const store = createStore(reducer);
-
-export { store };
