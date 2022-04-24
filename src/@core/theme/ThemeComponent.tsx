54775618b2da2055a3648a8e21dd64e41c0b5324
Lovish Tater <lovishtater@gmail.com>
2022-04-24 15:19:43 +0530

    added color theme

---
+++ src/@core/theme/ThemeComponent.tsx
@@ -0,0 +1,50 @@
+
+import { ReactNode } from 'react'
+
+
+import CssBaseline from '@mui/material/CssBaseline'
+import GlobalStyles from '@mui/material/GlobalStyles'
+import { ThemeProvider, createTheme, responsiveFontSizes } from '@mui/material/styles'
+
+// ** Type Imports
+import { Settings } from 'src/@core/context/settingsContext'
+
+// ** Theme Config
+import themeConfig from 'src/configs/themeConfig'
+
+// ** Theme
+import themeOptions from './ThemeOptions'
+
+// ** Global Styles
+import GlobalStyling from './globalStyles'
+
+interface Props {
+  settings: Settings
+  children: ReactNode
+}
+
+const ThemeComponent = (props: Props) => {
+  // ** Props
+  const { settings, children } = props
+
+  // ** Merged ThemeOptions of Core and User
+  const coreThemeConfig = themeOptions(settings)
+
+  // ** Pass ThemeOptions to CreateTheme Function to create partial theme without component overrides
+  let theme = createTheme(coreThemeConfig)
+
+  // ** Set responsive font sizes to true
+  if (themeConfig.responsiveFontSizes) {
+    theme = responsiveFontSizes(theme)
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <GlobalStyles styles={() => GlobalStyling(theme) as any} />
+      {children}
+    </ThemeProvider>
+  )
+}
+
+export default ThemeComponent
