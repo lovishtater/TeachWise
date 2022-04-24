1ae45420efb140676a806d969f05ccf4778ab524
Lovish Tater <lovishtater@gmail.com>
2022-04-24 15:23:29 +0530

    config changes, Mui/lab removed

--- src/configs/themeConfig.ts
+++ src/configs/themeConfig.ts
@@ -1,7 +1,4 @@
-
 import { PaletteMode } from '@mui/material'
-
-// ** Types
 import { ContentWidth } from 'src/@core/layouts/types'
 
 type ThemeConfig = {
@@ -20,15 +17,9 @@ const themeConfig: ThemeConfig = {
   templateName: 'TeachWise',
   mode: 'light',
   contentWidth: 'boxed' /* full | boxed */,
-
-  // ** Routing Configs
   routingLoader: true /* true | false */,
-
-  // ** Navigation (Menu) Configs
   menuTextTruncate: true /* true | false */,
   navigationSize: 260 /* Number in PX(Pixels) /*! Note: This is for Vertical navigation menu only */,
-
-  // ** Other Configs
   responsiveFontSizes: true /* true | false */,
   disableRipple: false /* true | false */
 }
