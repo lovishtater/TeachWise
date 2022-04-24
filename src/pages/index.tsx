12c8bfca0e3c1b08f17116e4112aaba95cd05b99
Lovish Tater <lovishtater@gmail.com>
2022-04-24 15:20:22 +0530

    Index and login page

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+const Home: NextPage = () => {
+  return (
+    <div>
+      <main>
+        <h1>
+          Welcome to <a>TeachWise</a>
+        </h1>
+      </main>
+    </div>
+  )
+}
+
+export default Home
