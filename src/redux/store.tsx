b4e9bb10cdc11a3cdc318609790b118c1ff48b1d
Lovish Tater <lovishtater@gmail.com>
2022-05-15 02:19:12 +0530

    Redux Store Updated

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,10 +1,19 @@
-import { createStore, applyMiddleware, compose } from "redux"
+import { applyMiddleware, compose } from "redux"
+import {configureStore } from "@reduxjs/toolkit"
 import thunk from "redux-thunk"
 import { createWrapper } from "next-redux-wrapper"
 import rootReducer from "./reducers/rootReducer"
 
 const middleware = [thunk]
 
-const makeStore = () => createStore(rootReducer, compose(applyMiddleware(...middleware)))
+
+//old code 
+// const makeStore = () => createStore(rootReducer, compose(applyMiddleware(...middleware)))
+
+
+const makeStore = () => configureStore({
+    reducer: rootReducer,
+    middleware: [...middleware]
+})
 
 export const wrapper = createWrapper(makeStore)
